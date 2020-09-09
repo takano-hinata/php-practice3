104a2b2b2889e23d9ac2b1c47d237df17d073e41
EC2 Default User <ec2-user@ip-172-31-40-51.us-east-2.compute.internal>
2020-09-09 08:58:09 +0000

    first commit

---
+++ practice3.php
@@ -0,0 +1,70 @@
+<?php 
+
+
+//課題１
+function sum1($max) {
+    $result = $max * 2;
+    echo $result . "\n";
+}
+
+sum1(4);
+
+
+//課題２
+function sum2($a, $b) {
+    echo $a * $b . "\n";
+}
+
+sum2(2,2);
+
+
+//課題３
+function times_array($arr) {
+    $result = 1;
+    foreach($arr as $a) {
+        $result *= $a;
+    }
+    return $result . "\n";
+}
+
+echo times_array(array(1, 3, 5 ,7, 9));
+
+
+//課題４
+function max_array($arr) {
+ $max_number = $arr[0];
+ foreach($arr as $a){
+     if ($max_number < $a) {
+         $max_number = $a;
+        }
+    }
+ return $max_number . "\n";
+}
+ 
+echo max_array(array(1, 3, 5 ,7, 9));
+
+
+//課題５
+//・strip_tags
+$text = "<h1>サーモン</h1><p>イクラ</p><strong>ウニ</strong>";
+echo $text . "\n";
+echo strip_tags($text,'<p>') . "\n";
+
+//・array_push
+$members = array("マグロ","イカ","アジ");
+array_push($members,"ブリ","サンマ");
+print_r($members);
+
+//・array_merge
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+$array3 = [7, 8, 9];
+$array = array_merge($array1,$array2,$array3);
+print_r($array);
+
+//・time
+$time1 = time();
+echo $time1 . "\n";
+
+//・mktime, date
+echo date("Y年m月d日　H時i分" , mktime(0,0,0,1,1,2000));
